0ef180db6134c8996bb6996ec08a7af61e4da3ad
Kioko <kllk.spb@mail.ru>
2020-06-04 15:05:28 +0300

    Second commit

--- Palindrome.java
+++ Palindrome.java
@@ -2,38 +2,39 @@ import java.util.Scanner;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String exitword = "";
-        Scanner in = new Scanner(System.in);
+        String exitword = "";   //Переменная для цикла выхода
+        Scanner in = new Scanner(System.in);    //Scanner для записи слов из консоли
         do {
             System.out.print("Input word: ");
-            String word = in.nextLine();
-                if (isPalindrome(word)) {
-                    System.out.println(word + " is a palindrome");
+            String word = in.nextLine();    //Записываем в новую переменную слово из консоли введенное пользователем
+                if (isPalindrome(word)) {   //Если метод isPalindrome = true
+                    System.out.println(word + " is a palindrome");  
                     System.out.println("New word? yes/exit");
-                    exitword = in.nextLine();
-                } else {
+                    exitword = in.nextLine();   //Вводим yes или exit, для зацикливания программы, если нужно ввести новое слово
+                } else {    //Если метод = false
                     System.out.println(word + " isn't a palindrome");
                     System.out.println("New word? yes/exit");
-                    exitword = in.nextLine();
+                    exitword = in.nextLine();   //Так же вводим для зацикливания
                 }
-        } while (exitword.equals("yes"));
-        in.close();
+        } while (exitword.equals("yes"));   //Пока пользователь вводи yes, цикл повторяется
+        in.close(); //Закрываем Scanner
     }
-    public static String reverse(String reverseString) {
-        String rev = "";
 
-        for (int i = reverseString.length() - 1; i >= 0; i--) {
-            rev = rev + reverseString.charAt(i);
+    public static String reverse(String reverseString) {    //Метод для реверса слова
+        String rev = "";    //Создаю пустую переменную rev, чтобы в неё записать изменённое слово
+
+        for (int i = reverseString.length() - 1; i >= 0; i--) { //Выбераю в массиве последнюю букву
+            rev = rev + reverseString.charAt(i);    //Записываю последнюю букву в начало
         }
 
-        return rev;
+        return rev; //Возвращаю изменённое слово
     }
-    public static boolean isPalindrome(String s) {
-        String rev = reverse(s);
-        if (rev.equals(s)) {
-            return true;
+    public static boolean isPalindrome(String s) {  //Метод для сравнивания перевернутого слова и нормального
+        String rev = reverse(s);    //Записываю в новую переменную rev перевёрнутое слово из метода reverse
+        if (rev.equals(s)) {    //Если перевёрнутое слово схоже с нормальным
+            return true;    //Возращаю true
         } else {
-            return false;
+            return false;   //Возвращаю false
         }
     }
 } 
--- Primes.java
+++ Primes.java
@@ -1,20 +1,21 @@
 public class Primes {
     public static void main(String[] args) {
         int out = 100;
-        for (int i = 2; i < out; i++) {
-            if (isPrime(i))
+        for (int i = 2; i < out; i++) {     //Для чисел от 2 до 100
+            if (isPrime(i))     //Если число простое, то выводим его
                 System.out.println(i);
         }
     }
 
-    public static boolean isPrime(int i) {
-        if (i<=1)
+    public static boolean isPrime(int i) {  //Метод для проверки чисел на простоту
+        if (i<=1)   //Если число меньше или равно 1, то число не простое, выводим false
             return false;
-        else if (i <= 3)
+        else if (i <= 3)    //Если число меньше или равно 3, то число простое
             return true;
-        else if (i % 2 == 0 || i % 3 == 0)
+        else if (i % 2 == 0 || i % 3 == 0)  //Если остаток от деления на 2 или 3 равно 0, то число не простое
             return false;
         int n = 5;
+        //Использую поиск по методу 6k+i
         while (n * n <= i) {
             if (i % n == 0 || i % (n + 2) == 0)
                 return false;
@@ -22,4 +23,4 @@ public class Primes {
         }
         return true;
     }
-} 
+}
